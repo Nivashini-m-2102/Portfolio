da527686a0a269629e4555b76ebe1cf70ce20dc1
Nivashini-m-2102 <nivashinimuthukumar21@gmail.com>
2026-06-04 21:36:02 +0530

    app file changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Project from "./components/Project";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import About from "./components/about";
+import Project from "./components/project";
 import BeyondCoding from "./components/BeyondCoding";
-import LeetCode from "./components/LeetCode";
-import Events from "./components/Events";
-import Certifications from "./components/Certifications";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
+import LeetCode from "./components/leetcode";
+import Events from "./components/events";
+import Certifications from "./components/certifications";
+import Contact from "./components/contact";
+import Footer from "./components/footer";
 function Home() {
   return (
     <>
